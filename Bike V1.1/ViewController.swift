a358689acc278c31e048e705bb7110716debae29
David Cai <davidcai2012@gmail.com>
2016-06-28 16:31:42 -0700

    Initial Commit

---
+++ Bike V1.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bike V1.1
+//
+//  Created by David Cai on 6/28/16.
+//  Copyright © 2016 David Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
